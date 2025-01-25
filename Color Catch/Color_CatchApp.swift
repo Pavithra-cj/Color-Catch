a6658f39830695fbd24c3ee74ec26d44d2970854
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2025-01-25 23:18:33 +0530

    Initial Commit

---
+++ Color Catch/Color_CatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_CatchApp.swift
+//  Color Catch
+//
+//  Created by Pavithra Chamod on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct Color_CatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color CatchTests/Color_CatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  Color_CatchTests.swift
+//  Color CatchTests
+//
+//  Created by Pavithra Chamod on 2025-01-25.
+//
+
+import Testing
+@testable import Color_Catch
+
+struct Color_CatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
